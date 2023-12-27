ffd167cdeb74e2f4f616f0a8132f80d7171da419
lost-chick <753402868@qq.com>
2023-12-27 15:23:24 +0800

    初始化

---
+++ src/pages/Organization/data.ts
@@ -0,0 +1,213 @@
+import avadefault from './imgs/ava-default.png'
+
+
+const data = {
+    id: 'root',
+    value: {
+        name: '李士雄',
+        title: '第一代',
+        icon: avadefault,
+    },
+    children: [
+        {
+            id: '2-1',
+            value: {
+                name: '李 升',
+                title: '第二代',
+                icon: avadefault,
+
+            },
+            children: [
+                {
+                    id: '3-1',
+                    value: {
+                        name: '李 义',
+                        title: '第三代',
+                        icon: avadefault,
+
+                    },
+                    children: [
+                        {
+                            id: '4-1',
+                            value: {
+                                name: '李德林',
+                                title: '第四代',
+                                icon: avadefault,
+                            },
+                        },
+                        {
+                            id: '4-2',
+                            value: {
+                                name: '李德山',
+                                title: '第四代',
+                                icon: avadefault,
+                            },
+                        },
+                        {
+                            id: '4-3',
+                            value: {
+                                name: '李德海',
+                                title: '第四代',
+                                icon: avadefault,
+                            },
+                            children: [
+                                {
+                                    id: '5-1',
+                                    value: {
+                                        name: '李 强',
+                                        title: '第五代',
+                                        icon: avadefault,
+                                    },
+                                },
+                                {
+                                    id: '5-2',
+                                    value: {
+                                        name: '李 凤',
+                                        title: '第五代',
+                                        icon: avadefault,
+                                    },
+                                    children: [
+                                        {
+                                            id: '6-1',
+                                            value: {
+                                                name: '李 金',
+                                                title: '第六代',
+                                                icon: avadefault,
+                                            },
+                                        },
+                                        {
+                                            id: '6-2',
+                                            value: {
+                                                name: '李 玉',
+                                                title: '第六代',
+                                                icon: avadefault,
+                                            },
+                                            children: [
+                                                {
+                                                    id: '7-1',
+                                                    value: {
+                                                        name: '李汝椁',
+                                                        title: '第七代',
+                                                        icon: avadefault,
+                                                    },
+                                                    children: [
+                                                        {
+                                                            id: '8-1',
+                                                            value: {
+                                                                name: '李汝艾',
+                                                                title: '第八代',
+                                                                icon: avadefault,
+                                                            },
+                                                        },
+                                                        {
+                                                            id: '8-2',
+                                                            value: {
+                                                                name: '李逢艾',
+                                                                title: '第八代',
+                                                                icon: avadefault,
+                                                            },
+                                                        },
+                                                    ],
+                                                },
+                                                {
+                                                    id: '7-2',
+                                                    value: {
+                                                        name: '李汝湘',
+                                                        title: '第七代',
+                                                        icon: avadefault,
+                                                    },
+                                                    children: [
+                                                        {
+                                                            id: '8-3',
+                                                            value: {
+                                                                name: '李逢秋',
+                                                                title: '第八代',
+                                                                icon: avadefault,
+                                                            },
+                                                        },
+                                                    ],
+                                                },
+                                                {
+                                                    id: '7-3',
+                                                    value: {
+                                                        name: '李汝杨',
+                                                        title: '第七代',
+                                                        icon: avadefault,
+                                                    },
+                                                    children: [
+                                                        {
+                                                            id: '8-4',
+                                                            value: {
+                                                                name: '李逢年',
+                                                                title: '第八代',
+                                                                icon: avadefault,
+                                                            },
+                                                        },
+                                                    ],
+                                                },
+                                            ],
+                                        },
+                                        {
+                                            id: '6-3',
+                                            value: {
+                                                name: '李 宝',
+                                                title: '第六代',
+                                                icon: avadefault,
+                                            },
+                                            children: [
+                                                {
+                                                    id: '7-5',
+                                                    value: {
+                                                        name: '李汝檀',
+                                                        title: '第七代',
+                                                        icon: avadefault,
+                                                    },
+                                                },
+                                                {
+                                                    id: '7-6',
+                                                    value: {
+                                                        name: '李汝',
+                                                        title: '第七代',
+                                                        icon: avadefault,
+                                                    },
+                                                    children: [
+                                                        {
+                                                            id: '8-5',
+                                                            value: {
+                                                                name: '李逢岩',
+                                                                title: '第八代',
+                                                                icon: avadefault,
+                                                            },
+                                                        },
+                                                        {
+                                                            id: '8-6',
+                                                            value: {
+                                                                name: '李逢时',
+                                                                title: '第八代',
+                                                                icon: avadefault,
+                                                            },
+                                                        },
+                                                    ],
+                                                },
+                                            ],
+                                        },
+                                    ],
+                                },
+                                {
+                                    id: '5-3',
+                                    value: {
+                                        name: '李 鸾',
+                                        title: '第五代',
+                                        icon: avadefault,
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
+export default data
---
+++ src/pages/Organization/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { OrganizationGraph, ShapeCfg } from '@ant-design/graphs';
+import { PageContainer } from '@ant-design/pro-components';
+import data from './data'
+import style from './index.less'
+
+const DemoOrganizationGraph = () => {
+
+    const calcStrLen = function calcStrLen(str) {
+        var len = 0;
+        for (var i = 0; i < str.length; i++) {
+            if (str.charCodeAt(i) > 0 && str.charCodeAt(i) < 128) {
+                len++;
+            } else {
+                len += 2;
+            }
+        }
+        return len;
+    };
+    return <div className={style['organization-container']}>
+        <div className={style['title']}>
+            李氏族谱
+        </div>
+        <OrganizationGraph
+            height={800}
+            data={data}
+            behaviors={['drag-canvas', 'zoom-canvas', 'scroll-canvas']}
+            nodeCfg={{
+                autoWidth: true,
+                padding: 10,
+                style: (node) => {
+                    return node.id === 'root'
+                        ? {
+                            fill: '#fff',
+                            stroke: '#ccc',
+                            cursor: 'pointer',
+                        }
+                        : { cursor: 'pointer', stroke: '#ccc', };
+                },
+                label: {
+                    style: (node, group, type) => {
+                        const styles = {
+                            icon: {
+                                width: 32,
+                                height: 32,
+                            },
+                            title: {
+                                fill: '#666',
+                            },
+                            name: {
+                                fontSize: 16,
+                                fontWeight: 'bold',
+                                fill: '#333',
+                            },
+                        };
+                        return styles[type];
+                    },
+                },
+
+            }}
+            markerCfg={(cfg) => {
+                return {
+                    position: 'bottom',
+                    show: cfg.children && cfg.children.length
+                }
+            }}
+            onReady={(graph) => {
+                graph.on('node:click', (evt) => {
+                    console.log(evt);
+                });
+            }}
+        />;
+    </div>
+};
+export default DemoOrganizationGraph;
