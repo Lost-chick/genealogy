74e9a81d72808590d3e63ff2de16271bbbdbf6e0
lost-chick <753402868@qq.com>
2023-12-27 16:00:27 +0800

    更改名称

--- config/config.ts
+++ config/config.ts
@@ -79,7 +79,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: '李氏族谱',
   layout: {
     locale: true,
     ...defaultSettings,
