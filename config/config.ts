badb8d568982f7eee4bea08f83ecbbda37a392a9
lost-chick <753402868@qq.com>
2023-12-27 15:33:37 +0800

    更改配置

--- config/config.ts
+++ config/config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
    * @doc https://umijs.org/docs/api/config#hash
    */
   hash: true,
-
+  publicPath: process.env.NODE_ENV === 'production' ? '/genealogy/' : '/',
   /**
    * @name 兼容性设置
    * @description 设置 ie11 不一定完美兼容，需要检查自己使用的所有依赖
